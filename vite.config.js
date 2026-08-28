1861b9f554e00ef034f2c41430ebb84ca17091cb
Ubuntu <abhishek@Azure-Node-01.a45ku2asyzfuxaxyjk2w0adrcg.bx.internal.cloudapp.net>
2026-08-28 19:00:25 +0000

    config: add GitHub Pages base path /polymarket-extractor/

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { getPlugin } from "./src/services/gammaApi.js";
 
 export default defineConfig({
+  base: "/polymarket-extractor/",
   plugins: [react(), getPlugin()],
   build: {
     outDir: "dist",
